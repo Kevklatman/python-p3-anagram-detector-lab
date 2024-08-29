0a51f47b5e1097902079dd459c0ba914fd84976b
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-29 00:03:52 -0600

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.word_sorted = ''.join(sorted(word.lower()))
+    
+    def match(self, candidates):
+        anagrams = []
+        for candidate in candidates:
+            if self.is_anagram(candidate):
+                anagrams.append(candidate)
+        return anagrams
+    
+    def is_anagram(self, candidate):
+        if self.word.lower() == candidate.lower():
+            return False
+        candidate_sorted = ''.join(sorted(candidate.lower()))
+        return self.word_sorted == candidate_sorted
